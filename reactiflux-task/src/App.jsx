3d39426ac33490974fce42cb9c2ca75f7fbb06e9
riiyaa <riyac.wot2022@gmail.com>
2023-07-04 11:17:38 +0530

    complete row, col, save, cancel, edit functonality

--- reactiflux-task/src/App.jsx
+++ reactiflux-task/src/App.jsx
@@ -6,30 +6,29 @@ import style from './App.module.scss';
 function App() {
 
   const [table, setTable] = useState({
-    rowInitial:[{
-      rowName:'',
-      columns:['']
-    }],
+    rowInitial:[['']],
+    initial:[['']],
     isDisable:true
   })
 
   const addRow = () =>{
     // let row = sessionStorage.getItem('row') ? sessionStorage.getItem('row') : 0
-    const val = structuredClone(table.rowInitial)
-    val.push({
-      rowName:'',
-      columns:val[0]?.columns
-    })
-      setTable(prev => ({...prev,rowInitial : val}))
-      // columns:[...prev.rowInitial.columns,'']  
+    // const val = structuredClone(table.rowInitial)
+    // val.push({
+    //   rowName:'',
+    //   columns:val[0]?.columns
+    // })
+    const count = table.rowInitial[0]?.length ?? 1
+    let arr = new Array(count)
+    setTable(prev => ({...prev,rowInitial : [...prev.rowInitial, arr.fill(1).map(() => '')],initial:[...prev.initial, arr.fill(1).map(() => '')]}))
   }
 
   const addCol = () =>{
     const val = structuredClone(table.rowInitial);
     val.map((res)=>{
-      res.columns.push('ffffffv')
+      res.push('')
     })
-    setTable(prev => ({...prev,rowInitial:val}))
+    setTable(prev => ({...prev,rowInitial:val,initial:val}))
   }
 
   const editRow = () =>{
@@ -39,28 +38,27 @@ function App() {
     }))
   }
 
-  const save = (i,e) =>{
-    const val = structuredClone(table.rowInitial);
-    val[i].rowName = e.target.value;
-    setTable(prev => ({...prev,rowInitial : val}))
+  const save = (i,e,colI) =>{
+    const changeVal = structuredClone(table.rowInitial);
+    changeVal[i][colI] = e.target.value;
+    setTable(prev => ({...prev,rowInitial : changeVal}))
   }
 
-  const saveChanges = (i,e) =>{
-    console.log(table);
-    setTable(prev => ({
-      ...prev,
-      isDisable : true
-    }))
+  const saveChanges = () =>{
+    setTable(prev => ({...prev,isDisable:true,initial:[...prev.rowInitial]}))
   }
 
-
+  const cancel = () =>{
+    setTable(prev => ({...prev,isDisable:true,rowInitial:[...prev.initial]}))
+  }
   return (
     <>
     <div className={`${style.flex} ${style['justify-content-center']}`}>
-      <button className={style.btn} onClick={()=>addRow()}> Add row</button>
-      <button className={style.btn} onClick={()=>addCol()}> Add Column</button>
+      <button className={style.btn} onClick={()=>addRow()} disabled={!table.isDisable}> Add row</button>
+      <button className={style.btn} onClick={()=>addCol()} disabled={!table.isDisable}> Add Column</button>
       {table.isDisable && (<button className={style.btn} onClick={() => editRow()}> Edit Row Name</button>)}
       {!table.isDisable && (<button className={style.btn} onClick={() => saveChanges()}> Save Changes</button>)}
+      {!table.isDisable && (<button className={style.btn} onClick={() => cancel()}> Cancel changes</button>)}
     </div>
     <br></br>
     <br></br>
@@ -68,9 +66,10 @@ function App() {
       <table border={1} style={{borderCollapse:'collapse'}}>
         <tbody>
           {table.rowInitial.map((data,index)=>{
-            return <tr key={index}><input defaultValue={data.rowName} disabled={table.isDisable} onChange={(e)=> save(index,e)}/>
-            {data.columns.map((res,ind)=>{
-              return <td key={ind}><input defaultValue={res} disabled={table.isDisable}/></td>
+            return <tr key={index}>
+            {data.map((col, colIndex) => {
+              return <td key={colIndex}><input defaultValue={col} value={col} disabled={table.isDisable} onChange={(e)=> save(index,e,colIndex)}/>
+              </td>
             })}
             </tr>
           })}
