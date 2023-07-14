5bb60d6dc8a061f6460c547930e1256d4f99c9d5
rchauhan471 <rchauhan471@rku.ac.in>
2023-07-14 17:30:00 +0530

    add operations

--- react-calc/src/App.js
+++ react-calc/src/App.js
@@ -14,31 +14,71 @@ const [input, setInput] = useState({
 
 const pressNum = (num,isNum) => {
   if(isNum){
-    console.log(input);
-    if(input.isOperator){
-      setInput(prev => ({...prev,first:num,isOperator:false }))
-    }else if(!input.isOperator){
-      if(input.first=='0'){
-        setInput(prev => ({...prev,first:num}))
-      }else{
-        let val = structuredClone(input.first)
-        val = String(val) + num
+    let val = structuredClone(input.first)
+    val = String(val) + num
+    if(num=='.'){
+      if(!input.first.includes('.')){
         setInput(prev => ({...prev,first:val}))
+      }else {return}
+    }else{
+      if(input.isOperator){
+        setInput(prev => ({...prev,first:num,isOperator:false }))
+      }else if(!input.isOperator){
+        if(input.first=='0'){
+          setInput(prev => ({...prev,first:num}))
+        }else{
+          setInput(prev => ({...prev,first:val}))
+        }
       }
     }
   }else{
     const val1 = structuredClone(input.first)
     const val2 = structuredClone(input.first) + num
-    if(input.firstNum == 0){
-      setInput(prev => ({...prev,second:val2,isOperator:true,operator:num,firstNum:val1}))
-    }else{
-      const fNum = Number(structuredClone(input.firstNum))
-      const sNum = Number(structuredClone(input.first))
-      if(input.operator=='+'){
-        setInput(prev => ({...prev,first:fNum+sNum,second:fNum + sNum + num,isOperator:true,operator:num,firstNum:fNum + sNum}))
+    if(!input.isOperator){
+      if(input.firstNum == 0 && input.operator==''){
+        console.log('called');
+        setInput(prev => ({...prev,second:val2,isOperator:true,operator:num,firstNum:val1}))
+      }else{
+        console.log('called else');
+        const fNum = Number(structuredClone(input.firstNum))
+        const sNum = Number(structuredClone(input.first))
+        console.log('num',fNum,sNum,input.operator);
+        if(input.operator=='+'){
+          setInput(prev => ({...prev,first:String(fNum+sNum),second:(fNum + sNum) + num,isOperator:true,operator:num,firstNum:fNum + sNum}))
+        }
+        if(input.operator=='-'){
+          setInput(prev => ({...prev,first:String(fNum-sNum),second:(fNum - sNum) + num,isOperator:true,operator:num,firstNum:fNum - sNum}))
+        }
+        if(input.operator=='×'){
+          setInput(prev => ({...prev,first:String(fNum*sNum),second:(fNum * sNum) + num,isOperator:true,operator:num,firstNum:fNum * sNum}))
+        }
+        if(input.operator=='÷'){
+          setInput(prev => ({...prev,first:String(fNum/sNum),second:(fNum / sNum) + num,isOperator:true,operator:num,firstNum:fNum / sNum}))
+        }
       }
+    }else{
+      setInput(prev => ({...prev,second:val2,isOperator:true,operator:num,firstNum:val1}))
     }
-    console.log(input,num);
+  }
+}
+
+const equal = () => {
+  if(input.second.includes('=')){
+    
+  }else if(!input.second.includes('=') && input.operator!=''){
+    let ele = structuredClone(input)
+    // if(input.operator=='+'){
+    //   setInput(prev => ({...prev,first:String(fNum+sNum),second:(fNum + sNum) + num,isOperator:true,operator:num,firstNum:fNum + sNum}))
+    // }
+    // if(input.operator=='-'){
+    //   setInput(prev => ({...prev,first:String(fNum-sNum),second:(fNum - sNum) + num,isOperator:true,operator:num,firstNum:fNum - sNum}))
+    // }
+    // if(input.operator=='×'){
+    //   setInput(prev => ({...prev,first:String(fNum*sNum),second:(fNum * sNum) + num,isOperator:true,operator:num,firstNum:fNum * sNum}))
+    // }
+    // if(input.operator=='÷'){
+    //   setInput(prev => ({...prev,first:String(fNum/sNum),second:(fNum / sNum) + num,isOperator:true,operator:num,firstNum:fNum / sNum}))
+    // }
   }
 }
 
@@ -126,10 +166,10 @@ const pressNum = (num,isNum) => {
                 <button className="btn" type="button" onClick={()=> pressNum('0',true)}>
                   0
                 </button>
-                <button className="btn" type="button">
+                <button className="btn" type="button" onClick={()=>pressNum('.',true)}>
                   .
                 </button>
-                <button className="btn equal" type="button">
+                <button className="btn equal" type="button" onClick={()=>equal()}>
                   =
                 </button>
               </div>
