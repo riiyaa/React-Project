1ae9a1d32b8c37644484d1115c74b6bcc12ddcb6
riiyaa <riyac.wot2022@gmail.com>
2023-07-18 11:58:52 +0530

    remove redux and store in calculator

--- react-calc/src/index.js
+++ react-calc/src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './Content/store';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider>
     <App />
   </Provider>
 );
